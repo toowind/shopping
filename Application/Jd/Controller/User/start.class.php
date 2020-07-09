70fc4857510435814e8f29622836261264098f37
toowind <23238136@qq.com>
2020-07-09 21:41:53 +0800

    对接订单侠接口-导入商品信息

---
+++ Application/Jd/Controller/User/start.class.php
@@ -0,0 +1,32 @@
+<?php
+/* 小程序静默授权*/
+
+namespace Jd\Controller\User;
+
+use Common\Controller\BaseController;
+use Common\Response\Response;
+use Jd\Service\User\UserAction;
+
+class start extends BaseController
+{
+    public function run() {
+        try {
+            $data = self::$_param;
+            $code = empty($data["code"]) ? '' : $data["code"];
+            if (!$code) {
+                $this->ajaxReturn(array('status' => 0, 'code' => '100044', 'msg' => 'code不存在'));
+                Response::outPutFail(1, 'code不存在');
+            }
+
+            $result = UserAction::wechatStart($code);
+            if (isset($result['code']) && $result['code'] == 0) {
+                Response::outPut($result['data']);
+            } else {
+                Response::outPutFail($result['code'], $result['msg']);
+            }
+        } catch (\Exception $e) {
+            Response::outPutFail($e->getCode(), $e->getMessage());
+        }
+    }
+
+}
